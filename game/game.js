10930622f7e4f25d35007c3b6dfe70f7c54f5475
Alex Cabrera <alex.cabrera@gmail.com>
2014-05-11 12:10:14 -0400

    pieces added

--- game/game.js
+++ game/game.js
@@ -2,10 +2,14 @@ var game = new Phaser.Game(600, 600, Phaser.AUTO, 'game', { preload: preload, cr
 
 function preload(){
     game.load.image('grid', 'res/grid.png');
+    game.load.image('redpiece', 'res/redpiece.png');
+    game.load.image('bluepiece', 'res/bluepiece.png');
 }
 
 function create(){
     game.add.sprite(0, 0, 'grid');
+    game.add.sprite(302, 254, 'redpiece');
+    game.add.sprite(254, 254, 'bluepiece');
 }
 
 
