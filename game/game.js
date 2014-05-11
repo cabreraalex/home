a92d8ade79f44e0819e4c3b2e2f8ffb5ea6c6edc
Alex Cabrera <alex.cabrera@gmail.com>
2014-05-11 15:20:54 -0400

    location detection

--- game/game.js
+++ game/game.js
@@ -17,29 +17,43 @@ function preload(){
 function create(){
     game.add.sprite(0, 0, 'grid');
 
-    redpawns[0] = game.add.sprite(302, 254, 'redpiece');
-    bluepawns[0] = game.add.sprite(254, 254, 'bluepiece');
-    redgenerals[0] = game.add.sprite(542, 254, 'redgeneral');
-    bluegenerals[0] = game.add.sprite(14, 254, 'bluegeneral');
-
-    for (var i = 0; i < 1; i++){
+    for (var i = 0; i < 8; i++){
+        redpawns[i] = game.add.sprite(555, 0, 'redpiece');
         redpawns[i].inputEnabled = true;
         redpawns[i].input.enableDrag();
         redpawns[i].input.enableSnap(48, 48, false, true, 14, 14);
+        redpawns[i].events.onDragStop.add(redFix);
+        bluepawns[i] = game.add.sprite(0, 0, 'bluepiece');
         bluepawns[i].inputEnabled = true;
         bluepawns[i].input.enableDrag();
         bluepawns[i].input.enableSnap(48,48, false, true, 14, 14);
+        bluepawns[i].events.onDragStop.add(blueFix);
     }
 
-    for (var i = 0; i < 1; i++){
+    for (var i = 0; i < 2; i++){
+        redgenerals[i] = game.add.sprite(0, 45, 'bluegeneral');
         redgenerals[i].inputEnabled = true;
         redgenerals[i].input.enableDrag();
-        redgenerals[i].input.enableSnap(48, 48, false, true, 14, 14);
+        redgenerals[i].input.enableSnap(48, 50, false, true, 14, 14);
+        bluegenerals[i] = game.add.sprite(555, 45, 'redgeneral');
         bluegenerals[i].inputEnabled = true;
         bluegenerals[i].input.enableDrag();
         bluegenerals[i].input.enableSnap(48, 48, false, true, 14, 14);
+        bluegenerals[i].events.onDragStop.add(blueFix);
     }
 }
 
 
 function update(){}
+
+function blueFix(item) {
+    if(item.x > 300) {
+        item.x = 254;
+    }
+}
+
+function redFix(item) {
+    if(item.x < 300) {
+        item.x = 300;
+    }
+}
