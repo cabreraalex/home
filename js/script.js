6aceb11b2653dc6a11e22385c1f35ca916862c61
Alex Cabrera <alex.cabrera@gmail.com>
2014-05-29 12:54:50 -0400

    fade

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
     $('#mainhead').css('margin-top', (($( document ).height()/2) - 200) + 'px');
+    $(function(){
+        $("#slides").slidesjs({
+            width: 500,
+            height: 18,
+            navigation: {
+                active: false,
+                effect: "fade"
+            },
+            pagination: {
+                active: false
+            }
+        });
+    });
 });
+
