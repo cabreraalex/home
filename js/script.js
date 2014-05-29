3ec124cf8ff297dba148d89c28322aeb49ff0501
Alex Cabrera <alex.cabrera@gmail.com>
2014-05-29 12:58:31 -0400

    fix

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $(function(){
         $("#slides").slidesjs({
             width: 500,
-            height: 18,
+            height: 25,
             navigation: {
                 active: false,
                 effect: "fade"
