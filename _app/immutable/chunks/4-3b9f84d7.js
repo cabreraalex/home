26706a1c729600c86f2e6d31065d194035bde681
Alex Cabrera <alex.cabrera@gmail.com>
2023-01-17 16:41:49 -0700

    Updates

---
+++ _app/immutable/chunks/4-3b9f84d7.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-11e2b142.js";import{default as t}from"../components/pages/(base)/_page.svelte-da038945.js";export{t as component,r as shared};
---
+++ _app/immutable/chunks/6-6dac794b.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-cd8510d9.js";import{default as t}from"../components/pages/cv/_page.svelte-e9e603e9.js";export{t as component,r as shared};
