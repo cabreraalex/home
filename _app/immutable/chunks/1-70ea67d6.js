248f8f59443a3db83b68e2ccc2c035d4ba01107d
Alex Cabrera <alex.cabrera@gmail.com>
2023-02-02 18:04:36 -0500

    Updates

---
+++ _app/immutable/chunks/1-70ea67d6.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-cc685135.js";export{t as component};
---
+++ _app/immutable/chunks/6-692b9b46.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-cd8510d9.js";import{default as t}from"../components/pages/cv/_page.svelte-e7530c80.js";export{t as component,r as shared};
