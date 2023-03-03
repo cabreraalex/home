c2d9d5f5538a05f4067749947897bd7c5def82f0
Alex Cabrera <alex.cabrera@gmail.com>
2023-03-03 17:26:52 -0500

    Updates

---
+++ _app/immutable/chunks/1-bf92137c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-5ab8aa6b.js";export{t as component};
--- _app/immutable/modules/pages/(base)/_layout.js-e992bf42.js
+++ _app/immutable/modules/pages/(base)/_layout.js-e992bf42.js
@@ -1,1 +1,1 @@
-import{p}from"../../../chunks/_layout-fca24c55.js";export{p as prerender};
+import{p}from"../../../chunks/_layout-da46b06b.js";export{p as prerender};
