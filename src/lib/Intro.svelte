f5ab7b9917b7bdcb982634073eb75186c6e27c82
Alex Cabrera <alex.cabrera@gmail.com>
2023-08-22 12:08:57 -0700

    candidate

--- src/lib/Intro.svelte
+++ src/lib/Intro.svelte
@@ -1,46 +1,16 @@
 <p>
-	I am a PhD student in the
+	I am a PhD candidate in the
 	<a href="https://hcii.cmu.edu/"> Human-Computer Interaction Institute (HCII) </a>
 	at Carnegie Mellon University, advised by
 	<a href="http://perer.org">Adam Perer</a>
 	and
 	<a href="http://www.cs.cmu.edu/~jasonh/">Jason Hong.</a>
 	I work on human-centered AI, specifically in applying techniques from HCI and visualization to help
-	people better understand and improve their machine learning models. I am supported by an
+	people better understand and improve their AI systems. I am supported by an
 	<a href="https://www.nsfgrfp.org/">NSF Graduate Research Fellowship</a>
-	and have spent time at
-	<!-- <img
-    style="width: 16px; padding-left: 5px; margin-bottom: -2px"
-    src="/images/microsoft.svg"
-    alt="microsoft" /> -->
-	Apple AI/ML, Microsoft Research, and Google.
-	<!-- <img
-    style="width: 16px; padding-left: 5px; margin-bottom: -2px"
-    src="/images/google.svg"
-    alt="google" /> -->
-	<!-- Google working on Google Maps, Cloud Dataflow, and Android Auto. -->
+	and have spent time at Apple AI/ML, Microsoft Research, and Google.
 </p>
 
-<!-- <p>
-  Before CMU, I graduated with a B.S. in Computer Science from Georgia Tech
-  where I worked with
-  <a href="https://www.cc.gatech.edu/~dchau/">Polo Chau</a>
-  and
-  <a href="http://jamiemorgenstern.com/">Jamie Morgenstern.</a>
-  I've spent time at -->
-<!-- <img
-    style="width: 16px; padding-left: 5px; margin-bottom: -2px"
-    src="/images/microsoft.svg"
-    alt="microsoft" /> -->
-<!-- Apple AI/ML, Microsoft Research, and a few summers as a software engineering intern
-  at -->
-<!-- <img
-    style="width: 16px; padding-left: 5px; margin-bottom: -2px"
-    src="/images/google.svg"
-    alt="google" /> -->
-<!-- Google working on Google Maps, Cloud Dataflow, and Android Auto. -->
-
-<!-- </p> -->
 <style>
 	p {
 		margin-bottom: 20px;
