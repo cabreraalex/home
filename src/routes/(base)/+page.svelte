e938752f974f5f032e30f36cdf8bc1b11cc4c3c1
Alex Cabrera <alex.cabrera@gmail.com>
2023-11-11 10:56:19 -0700

    update

--- src/routes/(base)/+page.svelte
+++ src/routes/(base)/+page.svelte
@@ -32,7 +32,7 @@
 		<hr />
 		{#each data.pubs as pub}
 			<div class="pure-g pub">
-				<div class="thumb-box pure-u-1 pure-u-md-7-24">
+				<div class="thumb-box pure-u-1 pure-u-md-1-4">
 					<a href={'/paper/' + pub.id}>
 						<div
 							style="background-image: url({'images/' + pub.teaser})"
@@ -42,19 +42,9 @@
 					</a>
 					<div class="image-caption">
 						<p>{pub.venue}</p>
-						{#if pub.award}
-							<div class="award-container">
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px"
-									><title>trophy</title><path
-										d="M18 2C17.1 2 16 3 16 4H8C8 3 6.9 2 6 2H2V11C2 12 3 13 4 13H6.2C6.6 15 7.9 16.7 11 17V19.08C8 19.54 8 22 8 22H16C16 22 16 19.54 13 19.08V17C16.1 16.7 17.4 15 17.8 13H20C21 13 22 12 22 11V2H18M6 11H4V4H6V11M20 11H18V4H20V11Z"
-									/></svg
-								>
-								<p class="award">{pub.award}</p>
-							</div>
-						{/if}
 					</div>
 				</div>
-				<div class="pure-u-1 pure-u-md-17-24">
+				<div class="pure-u-1 pure-u-md-3-4">
 					<div class="padded">
 						<a href={'/paper/' + pub.id}>
 							<h4 class="paper-title">{pub.title}</h4>
@@ -82,7 +72,7 @@
 		<hr />
 		{#each data.other as pub}
 			<div class="pure-g pub">
-				<div class="thumb-box pure-u-1 pure-u-md-7-24">
+				<div class="thumb-box pure-u-1 pure-u-md-1-4">
 					<a href={'/paper/' + pub.id}>
 						<div
 							style="background-image: url({'images/' + pub.teaser})"
@@ -92,7 +82,7 @@
 					</a>
 					<p class="venue">{pub.venue}</p>
 				</div>
-				<div class="pure-u-1 pure-u-md-17-24">
+				<div class="pure-u-1 pure-u-md-3-4">
 					<div class="padded">
 						<a href={'/paper/' + pub.id}>
 							<h4 class="paper-title">{pub.title}</h4>
@@ -121,15 +111,6 @@
 		align-items: center;
 		justify-content: space-between;
 		margin-right: 20px;
-	}
-	.award {
-		color: #b59410;
-		margin-left: 5px;
-	}
-	.award-container {
-		fill: #b59410;
-		display: flex;
-		align-items: center;
 	}
 	.thumb-box {
 		height: 100%;
